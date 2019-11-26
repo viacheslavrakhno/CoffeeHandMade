63c670eda424d8385d9118947a6e05dd17f5ada6
Viacheslav <viacheslavrakhno1993@gmail.com>
2019-11-26 13:08:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let person = {
+    name : "John",
+    age : 25,
+    isMarried : false
+};
+
+console.log(person["name"]);
+
+let arr = [ "orange.jpg","apple.png","opo.jpg"];
+console.log (arr[2]);
+
+//alert("Hello world!");
+
+let answer = prompt("How old are you?", "26");
+console.log(typeof(arr));
